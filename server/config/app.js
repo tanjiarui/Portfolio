fc136bc3cff2d62e122ef3e8689c6a1f113697f6
tanjiarui <tanjiarui@hotmail.com>
2020-10-20 22:07:10 +0800

    add authentication

--- server/config/app.js
+++ server/config/app.js
@@ -3,7 +3,11 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+// authentication setup
+const session = require('express-session');
+const passport = require('passport');
+const flash = require('connect-flash');
+const local_strategy = require('passport-local').Strategy;
 // database setup
 const mongoose = require('mongoose');
 const db = require('./db');
@@ -27,20 +31,45 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// setup session
+app.use(session({secret:'some',saveUninitialized:false,resave:false}));
+// initialize flash
+app.use(flash());
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// create a user instance
+const user_model = require('../models/user');
+const user = user_model.user;
+// authentication strategy
+passport.use(new local_strategy(
+  function(username, password, done) {
+    user.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (username !== user.username) { return done(null, false); }
+      if (password !== user.password) { return done(null, false); }
+      return done(null, user);
+    });
+  }
+));
+// serialize and deserialize the user info
+passport.serializeUser(user.serializeUser());
+passport.deserializeUser(user.deserializeUser());
+
 // bind static resource
 app.use('/public',express.static(path.join(__dirname, '../../public')));
-
 app.use('/', index_router);
 app.use('/contact', contact_router);
 
 // catch 404 and forward to error handler
-app.use((req, res, next) => {
-  next(createError(404));
-  res.render('error');
+app.use(function(res){
+	res.render('error')
 });
 
 // error handler
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- server/routes/contact.js
+++ server/routes/contact.js
@@ -1,26 +1,5 @@
 let express = require('express');
 let router = express.Router();
-<<<<<<< HEAD
-=======
-<<<<<<< Updated upstream
-
-let controller = require('../controllers/contact');
-
-/* GET Route for the contact page - read operation */
-router.get('/', controller.display_list);
-
-/* GET Route for displaying the edit page - update operation */
-router.get('/edit', controller.display_edit);
-router.get('/edit/:id', controller.display_edit);
-
-/* POST Route for processing the edit page - update operation */
-router.post('/edit/:id', controller.process_edit);
-
-/* GET to perform deletion - delete operation */
-router.get('/delete', controller.delete);
-router.get('/delete/:id', controller.delete);
-=======
->>>>>>> 4445f50d056d8f8930beca3f79033f0ba1988cd7
 let controller = require('../controllers/contact');
 
 // guard handler
@@ -47,9 +26,5 @@ router.post('/edit/:id', require_auth, controller.process_edit);
 /* GET to perform deletion - delete operation */
 router.get('/delete', require_auth, controller.delete);
 router.get('/delete/:id', require_auth, controller.delete);
-<<<<<<< HEAD
-=======
->>>>>>> Stashed changes
->>>>>>> 4445f50d056d8f8930beca3f79033f0ba1988cd7
 
 module.exports = router;
