4445f50d056d8f8930beca3f79033f0ba1988cd7
tanjiarui <tanjiarui@hotmail.com>
2020-10-20 21:55:48 +0800

    add authentication

--- server/routes/contact.js
+++ server/routes/contact.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let router = express.Router();
+<<<<<<< Updated upstream
 
 let controller = require('../controllers/contact');
 
@@ -16,5 +17,33 @@ router.post('/edit/:id', controller.process_edit);
 /* GET to perform deletion - delete operation */
 router.get('/delete', controller.delete);
 router.get('/delete/:id', controller.delete);
+=======
+let controller = require('../controllers/contact');
+
+// guard handler
+function require_auth(req,res,next)
+{
+  // judge login state
+  if(!req.isAuthenticated())
+  {
+    return res.redirect('/login');
+  }
+  next();
+}
+
+/* GET Route for the contact page - read operation */
+router.get('/', require_auth, controller.display_list);
+
+/* GET Route for displaying the edit page - update operation */
+router.get('/edit', require_auth, controller.display_edit);
+router.get('/edit/:id', require_auth, controller.display_edit);
+
+/* POST Route for processing the edit page - update operation */
+router.post('/edit/:id', require_auth, controller.process_edit);
+
+/* GET to perform deletion - delete operation */
+router.get('/delete', require_auth, controller.delete);
+router.get('/delete/:id', require_auth, controller.delete);
+>>>>>>> Stashed changes
 
 module.exports = router;
