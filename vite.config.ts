2cbb615ed4a3427bfde1480f78cb2dea3ca58ed7
Terry <tanjiarui@hotmail.com>
2023-11-22 07:38:14 -0500

    deployment path issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
+import {join} from 'path'
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue()],
-	base: 'portfolio'
+	resolve: {
+		alias: {
+			'@': join(__dirname, 'public')
+		}
+	}
 })
